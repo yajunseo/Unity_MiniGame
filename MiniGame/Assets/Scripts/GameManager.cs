71d55cd504c72dfbac756f63adc59bca5298709d
YAJUNSEO\yajunseo <yajunseo313@naver.com>
2023-08-30 17:50:53 +0900

    [미니게임/세팅/야준서] 프로젝트 세팅

---
+++ MiniGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+   protected override void Awake()
+    {
+        base.Awake();
+        // 추가적인 초기화 로직
+        // ...
+    }
+}
